e4f5ba6fc803650e4874a50abcdbbbf0748dfdec
vaibhavkudal <vaibhavkudal2006@gmail.com>
2026-03-04 20:32:20 +0530

    Create 4a_watermelon.c
    Codeforces Problem: Watermelon (4A)
    
    Given the weight of a watermelon, determine whether it can be split into two positive even parts. 
    If the weight is even and greater than 2, the answer is YES; otherwise, NO.

---
+++ 4a_watermelon.c
@@ -0,0 +1,221 @@
+By Vaibhav_Kudal, contest: Codeforces Beta Round 4 (Div. 2 Only), problem: (A) Watermelon, Accepted.
+
+Code:
+
+#include<stdio.h>
+int main()
+{
+    int w;
+scanf("%d",&w);
+if(w>2 && w%2==0)
+{
+    printf("YES\n");
+}
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
+
+→Judgement Protocol:
+
+Test: #1, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+8
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #2, time: 0 ms., memory: 72 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+5
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #3, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+4
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #4, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+3
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #5, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+2
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #6, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+1
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #7, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+7
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #8, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+6
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #9, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+10
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #10, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+9
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #11, time: 0 ms., memory: 20 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+53
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #12, time: 0 ms., memory: 20 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+77
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #13, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+32
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #14, time: 0 ms., memory: 8 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+44
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #15, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+98
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #16, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+99
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #17, time: 30 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+90
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #18, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+67
+Output
+NO
+Answer
+NO
+Checker Log
+ok answer is NO
+
+Test: #19, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+100
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
+
+Test: #20, time: 0 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+88
+Output
+YES
+Answer
+YES
+Checker Log
+ok answer is YES
